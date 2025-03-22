81d24964ae0ab68719bf441aacdf28e8f18d597d
santosh226206 <santoshamcl1996@gmail.com>
2025-03-22 20:48:39 +0530

    Initial commit for Kafka Producer Consumer

---
+++ src/main/java/com/kafkask/constant/ApplicationConstant.java
@@ -0,0 +1,9 @@
+package com.kafkask.constant;
+
+public class ApplicationConstant {
+	public static final String KAFKA_LOCAL_SERVER_CONFIG = "localhost:9092";
+	public static final String GROUP_ID_STRING = "group-id-string-1";
+	public static final String TOPIC_NAME = "mouser-topic";
+	public static final String KAFKA_LISTENER_CONTAINER_FACTORY = "kafkaListenerContainerFactory";
+	public static final String GROUP_ID_JSON = "group-id-json-1";
+}
